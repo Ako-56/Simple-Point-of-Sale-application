9f66fa257464447eb212ce482032f03fc332f643
Ako-56 <70592757+Ako-56@users.noreply.github.com>
2022-10-19 09:16:51 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,1 +1,17 @@
+<?php
+$server= "127.0.0.1:3306";
+$username = "root";
+$password = "*********";
+$dbname = "apos";
+
+
+// Create connection
+$conn = mysqli_connect($server, $username,$password,$dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
 
